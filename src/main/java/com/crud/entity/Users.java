93c6eaf2745de9cd2e2d2f5b4aac180eef30d121
madhurinaidu <96810168+madhurinaidu@users.noreply.github.com>
2024-10-15 11:28:55 +0530

    Update Users.java

--- src/main/java/com/crud/entity/Users.java
+++ src/main/java/com/crud/entity/Users.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import jakarta.annotation.Nonnull;
+import jakarta.annotation.Notnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
